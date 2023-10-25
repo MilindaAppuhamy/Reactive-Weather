c70d9276091bd3b0469256bf7c6e4b3a38910339
MilindaAppuhamy <milindaappuhamy.vodafone@gmail.com>
2023-10-25 10:30:34 +0100

    part 2 completed.

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import cities from "./data";
 import WeatherCard from "./components/WeatherCard";
+import Location from "./components/Location";
 
 function App() {
+  const [location, setLocation] = useState("London");
   return (
     <>
       <h1 className="title">REACTIVE WEATHER</h1>
       <h3 className="subtitle">Up to the minute weather news</h3>
       <div className="app">
-        {cities.map((city) => (
-          <WeatherCard data={city} />
-        ))}
+        <Location data={cities} location={location} setLocation={setLocation} />
       </div>
     </>
   );
--- components/Location.js
+++ components/Location.js
@@ -1,10 +1,13 @@
-import React from "react";
 import sunny from "../assets/Sunny.svg";
 import rainy from "../assets/Rainy.svg";
 import partlyCloudy from "../assets/PartlyCloudy.svg";
 import cloudy from "../assets/Cloudy.svg";
 
-function WeatherCard(props) {
+export default function Location(props) {
+  const { data, location, setLoaction } = props;
+
+  const city = data.find((elem) => elem.city === location);
+
   let image;
   if (props.data.forecast === "Sunny") {
     image = sunny;
@@ -27,12 +30,10 @@ function WeatherCard(props) {
         />
       </div>
       <div class="card-body">
-        <h3 className="card-title">{props.data.city}</h3>
-        <h5 className="card-text">{props.data.temperature}</h5>
-        <h5 className="card-text">{props.data.forecast}</h5>
+        <h3 className="card-title">The weather in {city.city} is,</h3>
+        <h5 className="card-text">{city.temperature} C</h5>
+        <h5 className="card-text">{city.forecast} day</h5>
       </div>
     </div>
   );
 }
-
-module.exports = WeatherCard;
